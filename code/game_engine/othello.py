cb29cb74945f8938830cdd270e3c0dfea5de33dc
BaconSun <pgsunbacon@gmail.com>
2016-10-10 14:58:05 +0800

    add an stupid AI...

--- code/game_engine/othello.py
+++ code/game_engine/othello.py
@@ -11,17 +11,17 @@ class othello_engine(object):
     If you want to see the showing of gameboard, set show = True
     """
     def __init__(self, parent = None, show = False, record = False):
-        self.pieces = ['x', 'o'] #pieces color
+        self.pieces = ['x', 'o', ' '] #pieces color
         self.show = show
         self.record = record
-        if (self.record):
+        if self.record:
             with open("Othello_Record.txt", "a+") as f:
                 time = date.now()
                 f.write("\n" + "--- "+ time.date + "-" + time.month + "-" + time.day + " " + time.hour + ":" + time.minute + ":" + time.second+ " ---\n")
                 f.close()
-        if (parent == None):
+        if parent == None:
             self.gameboard = []  # gameboard
-            self.currentplayer = 0
+            self.currentplayer = 0 # 0 is for black, 1 is for white
             self.stones = [0, 0] #stone number for each player
             self.validmoves = [0, 0] # possible moves for each player
             self.validboard = [[], []] # the possible moves's board for each player
@@ -29,46 +29,60 @@ class othello_engine(object):
             self.count = 0
             self.replay = ""
             self.restart()
+            self.winner = None # 0 for black, 1 for white, -1 for draw
+            self.last_flips = []
         else:
-            self.gameboard = cp.deepcopy(parent.get_board())  # gameboard
-            self.currentplayer = cp.deepcopy(parent.get_currentplayer, number = True)
-            self.stones = cp.deepcopy(parent.get_stones(), return_all = True) #stone number for each player
-            self.validmoves = cp.deepcopy(parent.get_validmoves(), return_all = True) # possible moves for each player
-            self.validboard = cp.deepcopy(parent.get_validboard(), return_all = True) # the possible moves's board for each player
+            self.gameboard = cp.deepcopy(parent.get_board())
+            self.currentplayer = cp.deepcopy(parent.get_currentplayer(number = True))
+            self.stones = cp.deepcopy(parent.get_stones(return_all = True))
+            self.validmoves = cp.deepcopy(parent.get_validmoves(return_all = True))
+            self.validboard = cp.deepcopy(parent.get_validboard(return_all = True))
             self.end = cp.deepcopy(parent.finished())
             self.count = cp.deepcopy(parent.get_count())
             self.replay = cp.deepcopy(parent.get_replay())
+            self.winner = cp.deepcopy(parent.get_winner())
 
     def get_board(self):
         return self.gameboard
 
+    def get_last_flips(self):
+        return self.last_flips
+
     def get_stones(self, player = None, return_all = False):
-        if (return_all):
+        if return_all:
             return self.stones
-        return self.stones[self.currentplayer]
+        if player == None:
+            player = self.currentplayer
+        return self.stones[player]
 
     def get_validboard(self, player = None, return_all = False):
-        if (return_all):
+        if return_all:
             return self.validboard
-        return self.validboard[self.currentplayer]
+        if player == None:
+            player = self.currentplayer
+        return self.validboard[player]
 
     def get_validmoves(self, player = None, return_all = False):
-        if (return_all):
+        if return_all:
             return self.validmoves
-        return self.validmoves[self.currentplayer]
+        if player == None:
+            player = self.currentplayer
+        return self.validmoves[player]
 
     def get_currentplayer(self, number = False):
-        if (number):
+        if number:
             return self.currentplayer
         else:
             return self.pieces[self.currentplayer]
+
     def get_count(self):
         return self.count
 
     def get_replay(self):
         return self.replay
 
-
+    def get_winner(self):
+        return self.winner
 
     def set_show(self, show):
         self.show = show
@@ -83,20 +97,35 @@ class othello_engine(object):
     def finished(self):
         return self.end
 
-
-    def __display(self, force = False):
-        if(force or self.show):
-            if (system() == 'Windows'):
+    def display(self, force = False, board = None):
+        if board == None:
+            board = self.validboard[self.currentplayer]
+        if force or self.show:
+            if system() == 'Windows':
                 sys("cls")
             else:
                 sys("clear")
             print "  0 1 2 3 4 5 6 7"
-            for i in range(8):
-                print str(i) + " " + ' '.join(self.gameboard[i])
+            for i in xrange(8):
+                print str(i) + " " + ' '.join(self.pieces[j] for j in board[i])
+            print
+            raw_input()
+
+    def __display(self, force = False, board = None):
+        if board == None:
+            board = self.gameboard
+        if force or self.show:
+            if system() == 'Windows':
+                sys("cls")
+            else:
+                sys("clear")
+            print "  0 1 2 3 4 5 6 7"
+            for i in xrange(8):
+                print str(i) + " " + ' '.join(self.pieces[j] for j in board[i])
             print
 
     def __record(self):
-        if (self.record):
+        if self.record:
             with open("Othello_Record.txt", "a+") as f:
                 f.write(str(self.count) + ": " + self.replay + "\n")
                 f.close()
@@ -108,21 +137,34 @@ class othello_engine(object):
         self.end = False
         self.currentplayer = 0
         if len(self.gameboard) == 0:
-            for i in range(8):
-                self.gameboard.append(list("________"))
-                self.validboard[0].append(list("________"))
-                self.validboard[1].append(list("________"))
+            for i in xrange(8):
+                self.gameboard.append([-1]*8)
+                self.validboard[0].append([-1]*8)
+                self.validboard[1].append([-1]*8)
         else:
-            for i in range(8):
-                self.gameboard[i] =list("________")
-                self.validboard[0][i] = list("________")
-                self.validboard[1][i] = list("________")
-        self.gameboard[3][3] = self.pieces[1]
-        self.gameboard[3][4] = self.pieces[0]
-        self.gameboard[4][3] = self.pieces[0]
-        self.gameboard[4][4] = self.pieces[1]
+            for i in xrange(8):
+                self.gameboard[i] =[-1]*8
+                self.validboard[0][i] = [-1]*8
+                self.validboard[1][i] = [-1]*8
+        self.gameboard[3][3] = 1
+        self.gameboard[3][4] = 0
+        self.gameboard[4][3] = 0
+        self.gameboard[4][4] = 1
         self.__validateAndCount()
         self.__display()
+
+
+    # This method is to undo one move. You should pass in the pieces been flipped in last turn
+    def undo_move(self, x, y, flips):
+        if self.gameboard[x][y] == -1:
+            print "Undo fail! No piece here!"
+            return
+        self.currentplayer = self.gameboard[x][y]
+        self.gameboard[x][y] = -1
+        for i, j in flips:
+            self.gameboard[i][j] = 1 - self.gameboard[i][j]
+        self.__validateAndCount()
+        self.end = False
 
     # Change the self.pieces in certain directions between target point and end point.
     # The @current_piece is the current color the target point, or assumed color of it during idnetifying whether the point will a valid move.
@@ -134,24 +176,25 @@ class othello_engine(object):
         listy = []
         num = 0
         found = False
-        while (not found and (i != end_x or j != end_y)):
-            if (end_x - i != 0):
+        while not found and (i != end_x or j != end_y):
+            if end_x - i != 0:
                 i = i + (end_x - i)/abs(end_x - i)
-            if (end_y - j != 0):
+            if end_y - j != 0:
                 j = j + (end_y - j)/abs(end_y - j)
-            if (self.gameboard[i][j] == '_'):
+            if self.gameboard[i][j] == -1:
                 return False
-            elif (self.gameboard[i][j] == current_piece):
+            elif self.gameboard[i][j] == current_piece:
                 found = True
                 break
             listx.append(i)
             listy.append(j)
             num = num + 1
-        if (num == 0 or not found):
+        if num == 0 or not found:
             return False
-        elif (change == True):
-            for n in range(num):
+        elif change == True:
+            for n in xrange(num):
                 self.gameboard[listx[n]][listy[n]] = current_piece
+                self.last_flips.append((listx[n], listy[n]))
         return True
 
     # Expand from the target point in 8 directions.
@@ -164,21 +207,21 @@ class othello_engine(object):
         left = min(target_x, 7 - target_y)
         right = min(7 - target_x, target_y)
 
-        if (self.__change_inside(target_x, target_y, 0, target_y, current_piece, change)):
+        if self.__change_inside(target_x, target_y, 0, target_y, current_piece, change):
             enable = True
-        if (self.__change_inside(target_x, target_y, 7, target_y, current_piece, change)):
+        if self.__change_inside(target_x, target_y, 7, target_y, current_piece, change):
             enable = True
-        if (self.__change_inside(target_x, target_y, target_x, 0, current_piece, change)):
+        if self.__change_inside(target_x, target_y, target_x, 0, current_piece, change):
             enable = True
-        if (self.__change_inside(target_x, target_y, target_x, 7, current_piece, change)):
+        if self.__change_inside(target_x, target_y, target_x, 7, current_piece, change):
             enable = True
-        if (self.__change_inside(target_x, target_y, target_x-up, target_y-up, current_piece, change)):
+        if self.__change_inside(target_x, target_y, target_x-up, target_y-up, current_piece, change):
             enable = True
-        if (self.__change_inside(target_x, target_y, target_x-left, target_y+left, current_piece, change)):
+        if self.__change_inside(target_x, target_y, target_x-left, target_y+left, current_piece, change):
             enable = True
-        if (self.__change_inside(target_x, target_y, target_x+down, target_y+down, current_piece, change)):
+        if self.__change_inside(target_x, target_y, target_x+down, target_y+down, current_piece, change):
             enable = True
-        if (self.__change_inside(target_x, target_y, target_x+right, target_y-right, current_piece, change)):
+        if self.__change_inside(target_x, target_y, target_x+right, target_y-right, current_piece, change):
             enable = True
 
         if (enable == True):
@@ -192,43 +235,47 @@ class othello_engine(object):
         self.stones[1] = 0
         self.validmoves[0] = 0
         self.validmoves[1] = 0
-        for i in range(8):
-            for j in range(8):
-                for k in range(2):
-                    self.validboard[k][i][j] = '_'
-                    if (self.gameboard[i][j] == '_'):
-                        if (self.__expand(i, j, self.pieces[k], False)):
-                            self.validboard[k][i][j] = self.pieces[k]
+        for i in xrange(8):
+            for j in xrange(8):
+                for k in xrange(2):
+                    self.validboard[k][i][j] = -1
+                    if self.gameboard[i][j] == -1:
+                        if self.__expand(i, j, k, False):
+                            self.validboard[k][i][j] = k
                             self.validmoves[k]+= 1
-                    elif (self.gameboard[i][j] == self.pieces[k]):
+                    elif self.gameboard[i][j] == k:
                         self.stones[k] += 1
 
     def update(self, x, y):
-        if (self.end == True):
+        if self.end == True:
             return
-        if (self.validboard[self.currentplayer][x][y] != self.pieces[self.currentplayer]):
+        if self.validboard[self.currentplayer][x][y] != self.currentplayer:
             self.__display(force = True)
             print "Invalid move!:", x, y
             return
-        self.gameboard[x][y] = self.pieces[self.currentplayer]
+        self.gameboard[x][y] = self.currentplayer
         self.replay = self.replay + " " + str(self.currentplayer) + str(x) + str(y)
         #update the self.gameboard
-        self.__expand(x, y, self.pieces[self.currentplayer], True)
+        self.last_flips = []
+        self.__expand(x, y, self.currentplayer, True)
         #update the self.validboard and self.validmoves
         self.__validateAndCount()
-        if (self.validmoves[1 - self.currentplayer] != 0):
+        if self.validmoves[1 - self.currentplayer] != 0:
             self.currentplayer = 1 - self.currentplayer
-        if(self.stones[0] == 0 or self.stones[1] == 0 or self.stones[0]+self.stones[1] == 64 or (self.validmoves[0]== 0 and self.validmoves[1] == 0)):
+        if self.stones[0] == 0 or self.stones[1] == 0 or sum(self.stones) == 64 or (self.validmoves[0]== 0 and self.validmoves[1] == 0):
             self.end = True
             self.__record()
             self.__display()
-            if (self.stones[0] > self.stones[1]):
+            if self.stones[0] > self.stones[1]:
+                self.winner = 0
                 self.replay = "Black" + self.replay
                 return "Black"
-            elif (self.stones[0] < self.stones[1]):
+            elif self.stones[0] < self.stones[1]:
+                self.winner = 1
                 self.replay = "White" + self.replay
                 return "White"
             else:
+                self.winner = -1
                 self.replay = "Draw" + self.replay
                 return "Draw"
         self.__display()
@@ -247,13 +294,20 @@ class othello_engine(object):
         fout.close()
 
 if  __name__ == '__main__':
-    cmd = raw_input("Start a new Game (y/n)? :")
+    cmd = raw_input("Start a new Game (y/n)? : ")
     engine = othello_engine(show = True)
-    while (cmd == "y" or cmd == "Y"):
-        while (not engine.finished()):
-            print "currentplayer is ", engine.get_currentplayer()
+    while cmd == "y" or cmd == "Y":
+        engine.restart()
+        while not engine.finished():
+            print "currentplayer is", engine.get_currentplayer()
             cmd = raw_input("input x, y: ")
-            if (len(cmd) > 2):
-                if (cmd[0].isdigit() and cmd[2].isdigit()):
+            if len(cmd) > 2:
+                if cmd[0].isdigit() and cmd[2].isdigit():
                     engine.update(ord(cmd[0]) - ord('0'), ord(cmd[2]) - ord('0'))
-        cmd = raw_input("Start a new Game (y/n)? :")
+        if engine.get_winner() == 0:
+            print "Winner is Black!"
+        elif engine.get_winner() == 1:
+            print "Winner is White!"
+        else:
+            print "Draw!"
+        cmd = raw_input("Start a new Game (y/n)? : ")
---
+++ code/game_engine/simple_ai.py
@@ -0,0 +1,172 @@
+from othello import othello_engine as oe
+from random import choice
+import copy as cp
+
+class ai_bot(object):
+    """
+    This AI bot use alpha beta search to do simple AI compute.
+    It heuristic function can be
+    1) a simple evalutation function by given points of certain pieces on corners and sides
+    2) a more complex evaluation function
+    3) a customed function passed in
+    """
+    def __init__(self, show = False, record = False, player = 1, heuristic = None, points = [10.5, 5.5], depth = 2, infinite = 1000):
+        if player == 1 or player.lower() == "white":
+            self.player = 1
+        else:
+            self.player = 0
+        if heuristic == None:
+            self.heuristic = self.simple_heuristic
+        elif heuristic == "complex":
+            self.heuristic = self.complex_heuristic
+        else:
+            self.heuristic = heuristic
+        self.points = points
+        self.depth = depth
+        self.infinite = infinite
+        self.game_engine = oe(show = show, record = record)
+
+    def get_engine(self):
+        return self.game_engine
+
+    def get_player(self):
+        return self.player
+
+    def set_player(self, player):
+        if player == 1 or player.lower() == "white":
+            self.player = 1
+        else:
+            self.player = 0
+
+    def run_best_move(self):
+        self.game_engine.set_show(False)
+        (x, y) = self.get_best_move()
+        self.game_engine.set_show(True)
+        self.game_engine.update(x, y)
+        return (x, y)
+
+    def get_best_move(self):
+        bestmove = []
+        bestresult = -self.infinite
+        moves = self.get_ordered_moves(self.game_engine, self.player)
+        if len(moves) == 0:
+            print "No Valid moves"
+            return None
+        for x, y in moves:
+            self.game_engine.update(x, y)
+            flips = cp.deepcopy(self.game_engine.get_last_flips())
+            result = self.alpha_beta(1-self.player, self.depth + sum(self.game_engine.get_stones(return_all = True))/20, bestresult, self.infinite)
+            self.game_engine.undo_move(x, y, flips)
+            if result > bestresult:
+                bestresult = result
+                bestmove = [(x, y)]
+            elif result == bestresult:
+                bestmove.append((x, y))
+        return choice(bestmove)
+
+    def get_ordered_moves(self, engine, player):
+        moves = []
+        if engine.get_validmoves(player = player) == 0:
+            return moves
+        validboard = engine.get_validboard(player = player)
+        for x, y in [(0,0),(0,7),(7,0),(7,7)]:
+            if validboard[x][y] == player:
+                moves.append((x,y))
+        for x in [0,7]:
+            for y in range(8)[1:-1]:
+                if validboard[x][y] == player:
+                    moves.append((x,y))
+                if validboard[y][x] == player:
+                    moves.append((y,x))
+        for x in range(8)[1:-1]:
+            for y in range(8)[1:-1]:
+                if validboard[x][y] == player:
+                    moves.append((x,y))
+        return moves
+
+    def simple_heuristic(self, engine, player):
+        board = engine.get_board()
+        value = 0
+        for x, y in [(0,0),(0,7),(7,0),(7,7)]:
+            if board[x][y] == player:
+                value += self.points[0]
+            else:
+                value -= self.points[0]
+        for x in [0,7]:
+            for y in range(8)[1:-1]:
+                if board[x][y] == player:
+                    value += self.points[1]
+                else:
+                    value -= self.points[1]
+                if board[y][x] == self.player:
+                    value += self.points[1]
+                else:
+                    value -= self.points[1]
+        for x in range(8)[1:-1]:
+            for y in range(8)[1:-1]:
+                if board[x][y] == player:
+                    value += 1
+                else:
+                    value -= 1
+        return value
+
+    def complex_heuristic(self):
+        return 0
+
+    def alpha_beta(self, player, depth, alpha, beta):
+        if depth == 0:
+            return self.heuristic(self.game_engine, player)
+        if self.game_engine.finished():
+            if self.game_engine.get_winner() == player:
+                return -self.infinite
+            elif self.game_engine.get_winner() == 1-player:
+                return self.infinite
+            else:
+                return 0
+        moves = self.get_ordered_moves(self.game_engine, player)
+        if len(moves) == 0:
+            result = self.alpha_beta(1-player, depth - 1, -beta, -alpha)
+            if result >= beta:
+                return beta
+            else:
+                return max(alpha, result)
+        for x, y in moves:
+            self.game_engine.update(x, y)
+            flips = cp.deepcopy(self.game_engine.get_last_flips())
+            result = self.alpha_beta(1-player, depth - 1, -beta, -alpha)
+            self.game_engine.undo_move(x, y, flips)
+            if result >= beta:
+                return beta
+            if result > alpha:
+                alpha = result
+        return alpha
+
+
+if __name__ == '__main__':
+    cmd = raw_input("Start a new Game (y/n)? : ")
+    bot = ai_bot(show = True)
+    while cmd == "y" or cmd == "Y":
+        bot.get_engine().restart()
+        cmd = raw_input("You want black(0)/white(1)? (default is black): ")
+        if cmd == "1" or cmd.lower() == "white":
+            bot.set_player(0)
+        else:
+            bot.set_player(1)
+        while not bot.get_engine().finished():
+            if bot.get_engine().get_currentplayer(number = True) != bot.get_player():
+                print "Your turn:", bot.get_engine().get_currentplayer()
+                cmd = raw_input("input x, y: ")
+                if len(cmd) > 2:
+                    if cmd[0].isdigit() and cmd[2].isdigit():
+                        bot.get_engine().update(ord(cmd[0]) - ord('0'), ord(cmd[2]) - ord('0'))
+            else:
+                (x,y) = bot.run_best_move()
+                print "The bot play on", x, y
+
+        if bot.get_engine().get_winner() == 0:
+            print "Winner is Black!"
+        elif bot.get_engine().get_winner() == 1:
+            print "Winner is White!"
+        else:
+            print "Draw!"
+        cmd = raw_input("Start a new Game (y/n)? : ")
